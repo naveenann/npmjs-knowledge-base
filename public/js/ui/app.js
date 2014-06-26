c6f6605442d367a6bf8aba81463eeb8551b776ad
Naveen.A.N <naveen.ann@gmail.com>
2014-06-26 21:21:19 +0530

    Refactor the structure

--- public/js/ui/app.js
+++
@@ -1,7 +0,0 @@
-$(window).on("resize", function () {
-    if($(window).width()<800) {
-        $(".side").css("display", "block");
-    }else {
-        $(".side").css("display", "none");
-    }
-}).resize();
